032eb77c833714e24c748f610d06f39a5258852b
Visualys <90142811+Visualys@users.noreply.github.com>
2022-02-22 18:23:59 +0100

    Update PB1_rf_send.c

--- PB1_rf_send.c
+++ PB1_rf_send.c
@@ -5,12 +5,12 @@ uint8_t PB1_rf_send(char* msg) {
     uint8_t LOWTIME = 32, HIGHTIME = 96;
     DDRB |= (1<<PB1);                      // set PB1 as output
     OCR0A = 128;
-    OCR0B = 32; 
-    TCCR0A = (1<<WGM01)|(1<<WGM00)|(1<<COM0B1);
+    OCR0B = HIGHTIME; 
+    TCCR0A = (1<<WGM01)|(1<<WGM00)|(1<<COM0B1)|(1<<COM0B0);
     TCNT0 = 0;                             //set counter to zero
     TCCR0B = (1<<WGM02) | 3;               //start timer0 with 1/64 speed (8MHz-> 1000 bauds)
     // preamble
-    for(b = 0; b < 8; b++) {
+    for(b = 0; b < 7; b++) {
         while((TIFR & (1<<TOV0))==0);      //wait counter 0 overflow
         TIFR |= (1<<TOV0);                 //reset overflow flag
         }
@@ -19,9 +19,9 @@ uint8_t PB1_rf_send(char* msg) {
         b=128;
         while(b) {
             if(msg[idx] & b) {
-                OCR0B = HIGHTIME;
-                }else{
                 OCR0B = LOWTIME;
+                }else{
+                OCR0B = HIGHTIME;
                 }
             b >>= 1;
             while((TIFR & (1<<TOV0))==0);  //wait counter 0 overflow
@@ -47,11 +47,14 @@ void wait_ms(uint32_t msec) {
     }
 
 int main(void) {
-    char s[50];
+    //char s[50];
     wait_ms(1000);
     while(1) {
-        s = "Test Message\r\n";
-        PB1_rf_send(s); 
-        wait_ms(1000);
+        
+        PB1_rf_send("Test Message\r\n"); 
+        wait_ms(500);
         }
     }
+
+ // avrdude -p ATtiny45 -c stk500 -P /dev/ttyACM0 -U flash:w:PB1_rf_send.hex:i -U lfuse:w:0xE2:m
+
