fdeda447d9870d050f164b289d74eb0768541409
Visualys <90142811+Visualys@users.noreply.github.com>
2022-02-24 15:03:26 +0100

    Update PB1_rf_send.c

--- PB1_rf_send.c
+++ PB1_rf_send.c
@@ -33,6 +33,41 @@ uint8_t PB1_rf_send(char* msg) {
     TCCR0B = 0;                            //stop timer0
     }
 
+uint8_t PB1_rf_transmit(char* msg) {
+    uint8_t b, idx = 0, bytescount = 4 + (msg[3] & 7);
+    uint8_t LOWTIME = 32, HIGHTIME = 96;
+    DDRB |= (1<<PB1);                      // set PB1 as output
+    OCR0A = 128;
+    OCR0B = HIGHTIME; 
+    TCCR0A = (1<<WGM01)|(1<<WGM00)|(1<<COM0B1)|(1<<COM0B0);
+    TCNT0 = 0;                             //set counter to zero
+    TCCR0B = (1<<WGM02) | 3;               //start timer0 with 1/64 speed (8MHz-> 1000 bauds)
+    // preamble
+    for(b = 0; b < 7; b++) {
+        while((TIFR & (1<<TOV0))==0);      //wait counter 0 overflow
+        TIFR |= (1<<TOV0);                 //reset overflow flag
+        }
+    // message
+    while(idx!=bytescount) {
+        b=128;
+        while(b) {
+            if(msg[idx] & b) {
+                OCR0B = LOWTIME;
+                }else{
+                OCR0B = HIGHTIME;
+                }
+            b >>= 1;
+            while((TIFR & (1<<TOV0))==0);  //wait counter 0 overflow
+            TIFR |= (1<<TOV0);             //reset overflow flag
+            }
+        idx++;
+        }
+    PORTB &= 0b11111101;
+    TCCR0B = 0;                            //stop timer0
+    }
+
+
+
 void wait_ms(uint32_t msec) {
     OCR0A = 128;
     TCCR0A = (1<<WGM01)|(1<<WGM00);
@@ -47,14 +82,22 @@ void wait_ms(uint32_t msec) {
     }
 
 int main(void) {
-    //char s[50];
+/*
     wait_ms(1000);
     while(1) {
-        
         PB1_rf_send("Test Message\r\n"); 
-        wait_ms(500);
+        wait_ms(1000);
         }
+*/
+
+    uint8_t msg[11];
+    msg[0]=0xBE;
+    msg[1]=0x01;
+    msg[2]=0x01;
+    msg[3]=0b10001001;
+    msg[3]=125;
+    while(1) {
+        PB1_rf_transmit(msg); 
+        wait_ms(1000);
+
     }
-
- // avrdude -p ATtiny45 -c stk500 -P /dev/ttyACM0 -U flash:w:PB1_rf_send.hex:i -U lfuse:w:0xE2:m
-
