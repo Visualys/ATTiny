98ab7ab0dc96d8b77e6d014874724879d8d3995b
Visualys <90142811+Visualys@users.noreply.github.com>
2022-02-25 10:17:32 +0100

    Update PB0_read_rf_inprogress.c

--- PB0_read_rf_inprogress.c
+++ PB0_read_rf_inprogress.c
@@ -21,36 +21,29 @@ while(1){
 		TCNT0 = 0;               // compteur à zero
 		while(!(PINB & DATA));   // attendre un signal HIGH
 		TCCR0B = 3;              //***  Lancer le compteur (0-255) (1/64clk)
-		//PORTB |= 0b00000010;
 		while( (PINB & DATA) && (!(TIFR & (1<<TOV0))) ); // laisser compter jusqu'a sig=BAS ou OVF
 		TCCR0B = 0;              //***  Stopper le compteur 
-		//PORTB &= 0b11111101;
 		if(TIFR & (1<<TOV0)) {
 			TIFR |= (1<<TOV0);                 //reset overflow flag
 			REG[0] = 0; REG_IDX = 0; BITS = 0; MAX_IDX = 10;
-			//PORTB ^= 0b00000010;
 		}else{
 			REG[REG_IDX] <<= 1;
-			if(TCNT0>63) REG[0]++;
+			if(TCNT0>63) REG[REG_IDX]++;
 			BITS++;
 			if(REG_IDX==0) {
 				BITS = 99;
 				if(REG[0]==0xBE) {REG_IDX++; BITS = 0;}
-				//if(REG[0]==0xBE) {REG_IDX++; BITS = 0; }
-			}else if(BITS==8) { 
+			    }
+
+			if(BITS==8) { 
+				if(REG_IDX==1) { if(REG[1]!=DEVICE_NUM) {REG[0] = 0; REG_IDX = 0; BITS = 0; MAX_IDX = 10; }}
+				if(REG_IDX==4) { MAX_IDX = 4 + (REG[3] & 7); }
 				REG_IDX++; BITS = 0;
 				}
-			if(BITS==0) {
-				if(REG_IDX==2) {
-					//if(REG[1]!=1) {REG[0] = 0; REG_IDX = 0; BITS = 0; MAX_IDX = 10; }
-					
-				}else if(REG_IDX==4) { 
-					MAX_IDX = 4 + (REG[3] & 7); 
-					}
-				}
+
 			}      
 		}
-	if(REG[0]==0xBE) PORTB ^= 0b00000010;
+	if(REG[1]==1) PORTB ^= 0b00000010;
 	}
 }
 
