2d048823c0ebc3f8b746d9c0796305b990c8e3b4
Visualys <90142811+Visualys@users.noreply.github.com>
2022-03-23 16:20:53 +0100

    Update test_calibrate.c

--- test_calibrate.c
+++ test_calibrate.c
@@ -5,11 +5,11 @@
 
 int main(void) {
 	char s[50];
-	DDRB |= (1<<PB0)|(1<<PB4);     // PB0 as output (oscillo), PB4 is TX
+	DDRB |= (1<<PB0)|(1<<PB4);                  // PB0 as output (oscillo), PB4 is TX
 	TCCR0A = (1<<COM0B0)|(1<<WGM01)|(1<<WGM00); // Fast PWM mode / Toggle COM.A
 	OCR0A = 99;
 	TCCR0B = (1<<WGM02) | 1;   // 1/1 clock 
-	PORTB |= (1 << PB1) | (1 << PB2);  // pull-up resistor on PB1 & PB2
+	PORTB |= (1 << PB1) | (1 << PB2);           // pull-up resistor on PB1 & PB2
 	while(1) {
 		if(!(PINB & PB1)){
 			wait_ms(10);
