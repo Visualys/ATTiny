15bd269b8411a53d6f05adab309f77255a47ea22
Visualys <90142811+Visualys@users.noreply.github.com>
2022-03-23 16:17:19 +0100

    Update test_calibrate.c

--- test_calibrate.c
+++ test_calibrate.c
@@ -9,8 +9,8 @@ int main(void) {
 	TCCR0A = (1<<COM0B0)|(1<<WGM01)|(1<<WGM00); // Fast PWM mode / Toggle COM.A
 	OCR0A = 99;
 	TCCR0B = (1<<WGM02) | 1;   // 1/1 clock 
+	PORTB |= (1 << PB1) | (1 << PB2);  // pull-up resistor on PB1 & PB2
 	while(1) {
-		PORTB |= (1 << PB1) | (1 << PB2);  // pull-up resistor on PB1 & PB2
 		if(!(PINB & PB1)){
 			wait_ms(10);
 			while(!(PINB & PB1));
