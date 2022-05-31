d1bc5dc9375f2469bcaf0dc3912dc8b0f9e53a37
Visualys <90142811+Visualys@users.noreply.github.com>
2022-05-31 18:05:34 +0200

    Update lib_wait.c

--- Tiny84/lib_wait.c
+++ Tiny84/lib_wait.c
@@ -6,8 +6,8 @@ void wait_ms(uint32_t msec) {
 	TCNT0 = 0;					// set counter to zero
 	TCCR0B = (1<<WGM02) | 3;			// start timer0 with 1/64 speed
 	while(msec > 0) {
-		while((TIFR & (1<<TOV0))==0);		// wait counter 0 overflow
-		TIFR |= (1<<TOV0);			// reset overflow flag
+		while((TIFR0 & (1<<TOV0))==0);		// wait counter 0 overflow
+		TIFR0 |= (1<<TOV0);			// reset overflow flag
 		msec--;
 		}
 	TCCR0B = 0;					// stop timer0
