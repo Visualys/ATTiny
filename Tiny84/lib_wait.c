2ebeecaa3ead7e8c003dbfed3c439fae27e76ddb
Visualys <90142811+Visualys@users.noreply.github.com>
2022-05-31 14:00:07 +0200

    Create lib_wait.c

---
+++ Tiny84/lib_wait.c
@@ -0,0 +1,14 @@
+// Calibrate the CPU frequency to 4 608 000 Hz
+
+void wait_ms(uint32_t msec) {
+	OCR0A = 71;
+	TCCR0A = (1<<WGM01)|(1<<WGM00);
+	TCNT0 = 0;					// set counter to zero
+	TCCR0B = (1<<WGM02) | 3;			// start timer0 with 1/64 speed
+	while(msec > 0) {
+		while((TIFR & (1<<TOV0))==0);		// wait counter 0 overflow
+		TIFR |= (1<<TOV0);			// reset overflow flag
+		msec--;
+		}
+	TCCR0B = 0;					// stop timer0
+	}
