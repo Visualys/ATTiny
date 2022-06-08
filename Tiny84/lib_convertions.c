024734be7cbd619ba5a2fc0de7db6dfb9a402ed5
Visualys <90142811+Visualys@users.noreply.github.com>
2022-06-08 12:03:42 +0200

    Update lib_convertions.c

--- Tiny84/lib_convertions.c
+++ Tiny84/lib_convertions.c
@@ -7,3 +7,18 @@ void stradd(char*s, char*s2){
 		}
 	s[n]=0;
 	}
+
+int strtoint(char*s){
+	int v=0, i;
+	uint8_t start=0, end=0;
+	while((s[start]<48 || s[start]>57) && s[start]>0) start++;
+	end=start;
+	while(s[end]>47 && s[end]<58) end++;
+	end--;
+	i=end;
+	while(i>=start){
+		v += (s[i]-48) * pow(10,(end-i));
+		i--;
+		}
+	return v;
+	}
