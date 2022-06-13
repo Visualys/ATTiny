4dd47e48adf036061339eaef623b16d91402e641
Visualys <90142811+Visualys@users.noreply.github.com>
2022-06-13 17:53:02 +0200

    Create linky_to_ttl.c

---
+++ Tiny84/linky_to_ttl.c
@@ -0,0 +1,50 @@
+#include <avr/io.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lib_wait.c"
+#include "lib_serial.c"
+
+
+uint16_t ADC_PA0_PA1vsVCC_read() {
+	uint16_t adc;
+	ADMUX = 0b00101000;						// read PA1+/PA0- voltage vs VCC ref.
+	ADCSRB = 0;								// unipolar
+	ADCSRA = 0b10000000;					// enable ADC
+	ADCSRA |= 0b01000000;					// start ADC
+	while(ADCSRA & 0b01000000);				// wait measure
+	adc = ADCL; adc |= ADCH << 8;
+	return adc;
+	}
+
+uint16_t adc;
+uint8_t maxreached=0;
+
+int main(void) {
+	OSCCAL = 13;
+	DDRA |= (1 << PA3); 					// PA3 --> TTL OUTPUT (TX)
+	OCR0A = 114;
+	TCCR0A = (1<<WGM01)|(1<<WGM00);
+	TCNT0 = 0;								// set counter to zero
+	TCCR0B = (1<<WGM02) | 2;				
+	while(1){
+		while(!(TIFR0 & (1<<TOV0))) {		// wait counter 0 overflow
+			adc = ADC_PA0_PA1vsVCC_read();
+			if(adc>512) maxreached=1;
+			}
+		if(maxreached){
+			PORTA &= ~(1 << PA3);
+		}else{
+			PORTA |= (1 << PA3);
+			}
+		TIFR0 |= (1<<TOV0);					// reset overflow flag
+		maxreached=0;
+		}
+	}
+
+
+
+/*
+avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:linky_to_ttl.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
+
+
+*/
