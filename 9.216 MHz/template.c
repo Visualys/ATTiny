bd2b158d832a3cd8b7ae838aa31d144eccf971f0
Visualys <90142811+Visualys@users.noreply.github.com>
2022-06-20 12:49:08 +0200

    Create template.c

---
+++ 9.216 MHz/template.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include "lib_serial.c"
+
+void main(void) {
+    eeprom_update_byte((uint8_t*) 1, 110 );                          // Set the OSCCAL memory address (line to remove when set)
+    OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
+    serial_send(PA0, "ATTiny84 started.\n", 115200);
+    }
+
+
+
+/*
+avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:template.hex:i -U lfuse:w:0xE2:m -U hfuse:w:0xD7:m
+*/
