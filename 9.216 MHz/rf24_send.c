83f341b0d0832b3e1db31a0148f6b924f6ceafd8
Visualys <90142811+Visualys@users.noreply.github.com>
2022-06-28 07:56:18 +0200

    Update rf24_send.c

--- 9.216 MHz/rf24_send.c
+++ 9.216 MHz/rf24_send.c
@@ -2,35 +2,33 @@
 #include <avr/eeprom.h>
 #include "lib_wait.c"
 #include "lib_serial.c"
-#include "lib_rf24.c"
+#include "lib_rf24_2.c"
 
-#define TX PA6
+#define TX PA5
 #define BAUDS 115200
 
 void main(void) {
-    //eeprom_update_byte((uint8_t*) 1, 110 );                        // Set the OSCCAL memory address (line to remove when set)
+    //eeprom_update_byte((uint8_t*) 1, 145 );                        // Set the OSCCAL memory address (line to remove when set)
     OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
     ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
     PRR |= ( 1 << PRADC );                                           // power off ADC
     serial_send(TX, "ATTiny84 started.\n", BAUDS);
 
-    char s[50];
+    char s[50]="ABC                             ";
     uint8_t loop;
-    s[0]=48;
-    s[1]=49;
-    s[2]=50;
-    s[3]=0;
-    rf24_init(PA0,PA1,PA2,PA3,PA4);                        // ce, cs, mosi, miso, clk
-    rf24_setconfig(2, 0, 0);                               // default channel
-    rf24_setaddress(0, 0xe7,0xe7,0xe7,0xe7,0xe7);          // default address
+    rf24_init(PA0,PA4,PA3,PA2,PA1);                         // ce, cs, mosi, miso, clk
+    rf24_setconfig(90, 0, 3);                               // default channel
+    rf24_setaddress(0, 100,100,100,100,100);                // default address
     rf24_setautoretransmit(15,15);
-    rf24_set_payload_length(3);
+    rf24_set_payload_length(32);
+    serial_send(TX, "Settings OK.\n", BAUDS);
     while(1){
         rf24_powerup_tx();
         rf24_send(s);
+        serial_send(TX, "Sending.\n", BAUDS);
         loop = 1;
         while(loop){
-            if(rf24_datasent()){
+           if(rf24_datasent()){
                 loop = 0;
                 serial_send(TX, "Message sent.\n", BAUDS);
                 }
@@ -39,6 +37,7 @@ void main(void) {
                 serial_send(TX, "Error : Max retries !\r\n", BAUDS);
                 }
             }
+        rf24_clear_status();
         rf24_powerdown();
         wait_ms(1000);
         }
@@ -47,5 +46,5 @@ void main(void) {
 
 
 /*
-avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:rf24_send.c.hex:i -U lfuse:w:0xE2:m -U hfuse:w:0xD7:m
+avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:rf24_send.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
 */
