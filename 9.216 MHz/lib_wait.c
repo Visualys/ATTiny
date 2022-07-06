e9767b54ecf177dd36e0a9a4640814ba7c883234
Visualys <90142811+Visualys@users.noreply.github.com>
2022-07-06 15:32:56 +0200

    Update lib_wait.c

--- 9.216 MHz/lib_wait.c
+++ 9.216 MHz/lib_wait.c
@@ -10,3 +10,16 @@ void wait_ms(uint32_t msec) {
         }
     TCCR0B = 0;                            // stop timer0
     }
+
+void wait_us(uint32_t usec) {
+    OCR0A = 8;
+    TCCR0A = (1<<WGM01)|(1<<WGM00);
+    TCNT0 = 0;                             // set counter to zero
+    TCCR0B = (1<<WGM02) | 1;               // start timer0 with 1/64 speed
+    while(msec > 0) {
+        while((TIFR0 & (1<<TOV0))==0);      // wait counter 0 overflow
+        TIFR0 |= (1<<TOV0);                 // reset overflow flag
+        usec--;
+        }
+    TCCR0B = 0;                            // stop timer0
+    }
