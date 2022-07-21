325e94a0f5b2c2017a642a472b6adf3933480783
Visualys <90142811+Visualys@users.noreply.github.com>
2022-07-21 22:14:19 +0200

    Update scan_osccal.c

--- 9.216 MHz/scan_osccal.c
+++ 9.216 MHz/scan_osccal.c
@@ -1,30 +1,39 @@
 #include <avr/io.h>
-#include <stdlib.h>
-#include "lib_wait.c"
+#include <avr/eeprom.h>
 #include "lib_serial.c"
+#include "lib_wait.c"
 #include "lib_str.c"
 
-int main(void) {
-    char s[50], v[10];
-    long n=0;
-    while(1) {
-        OSCCAL = n;
-        strset(s, "Valeur de OSCCAL : ");
-        ltoa(n, v, 10);
-        stradd(s, v);
-        stradd(s, "\n");
-        serial_send(PA0, s, 115200);
-        wait_ms(100);
-        n = (n + 1) & 0xFF;
+
+char s[50];
+uint32_t v=0;
+
+void main(void) {
+    eeprom_update_byte((uint8_t*) 1, 140 );                          // Set the OSCCAL memory address (line to remove when set)
+    OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
+    ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
+    PRR |= ( 1 << PRADC );                                           // power off ADC
+    serial_send(PA0, "ATTiny84 started.\n", 115200);
+    
+    
+    PORTA |=0b00000110;
+    while(1){
+		if(!(PINA & 0b00000010)){OSCCAL++;}
+		if(!(PINA & 0b00000100)){OSCCAL--;}
+		v=OSCCAL;
+		longtostr(v, s);
+		stradd(s,"\n");
+		serial_send(PA0, "OSCCAL = ", 115200);
+		serial_send(PA0, s, 115200);
+        wait_ms(1000);	
         }
     }
 
 
 /*
-with clock output on PB2 :
---------------------------
-avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:scan_osccal.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
-without clock output on PB2 :
------------------------------
-avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:scan_osccal.hex:i -U lfuse:w:0xE2:m -U hfuse:w:0xD7:m
+avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:template.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
 */
+
+
+
+
