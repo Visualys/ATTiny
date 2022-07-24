15aaf722cd336430f3d959a6570e9095a666a510
Visualys <90142811+Visualys@users.noreply.github.com>
2022-07-24 22:57:42 +0200

    Update i2c_lib.c

--- 9.216 MHz/i2c_lib.c
+++ 9.216 MHz/i2c_lib.c
@@ -17,56 +17,124 @@ I2C READ-WRITE : 0=Write, 1=Read
 --------------------------------------------------------------------------------------
 */
 
-uint8_t i2c_sda, i2c_scl;
+uint8_t i2c_sda, i2c_scl, i2c_address;
 
 void i2c_start() {
     PORTB |= (i2c_sda | i2c_scl);              // set all HIGH
-    wait_us(5);
+    wait_us(1);
     PORTB &= ~i2c_sda;
-    wait_us(5);
+    wait_us(1);
     PORTB &= ~i2c_scl;
-    wait_us(5);
+    wait_us(1);
     }
 
 void i2c_stop() {
     PORTB &= ~(i2c_sda | i2c_scl);             // set all LOW
-    wait_us(5);
+    wait_us(1);
     PORTB |= i2c_scl;
-    wait_us(5);
+    wait_us(1);
     PORTB |= i2c_sda;
-    wait_us(5);
+    wait_us(1);
     }
 
 uint8_t i2c_write(uint8_t byte){
     uint8_t i = 128, ret = 0;
-    i2c_start();
     while(i){
         if(i & byte){PORTB |= i2c_sda;}else{PORTB &= ~i2c_sda;}
-        wait_us(5);
+        wait_us(1);
         PORTB |= i2c_scl;
-        wait_us(5);
+        wait_us(1);
         PORTB &= ~i2c_scl;
-        wait_us(5);
+        wait_us(1);
         i >>= 1;
         }
     DDRB &= ~i2c_sda;                         // sda as input
     PORTB |= i2c_sda;                         // pull-up resistor
-    wait_us(5);
+    wait_us(1);
     PORTB |= i2c_scl;
-    wait_us(5);
+    wait_us(1);
     if(PINB & i2c_sda){ret=0;}else{ret=1;}
     PORTB &= ~i2c_scl;
-    wait_us(5);
+    wait_us(1);
     DDRB |= i2c_sda;                          // sda as output
+    return ret;
+    }
+    
+uint8_t i2c_read(){    
+    uint8_t i = 128, ret = 0;
+    DDRB &= ~i2c_sda;                         // sda as input
+    PORTB |= i2c_sda;                         // pull-up resistor
+    wait_us(1);
+    while(i){
+        PORTB |= i2c_scl;
+        wait_us(1);
+        if(PINB & i2c_sda){ret |=i;}
+        wait_us(1);
+        PORTB &= ~i2c_scl;
+        wait_us(1);
+        i >>= 1;
+        }
+    PORTB |= i2c_scl;
+    wait_us(1);
+                                              // NOACK
+    PORTB &= ~i2c_scl;
+    wait_us(1);
+    DDRB |= i2c_sda;                          // sda as output
+    return ret;
+	}
+
+uint8_t i2c_init(uint8_t sda, uint8_t scl, uint8_t address){
+	uint8_t ret;
+    i2c_sda = (1 << sda);
+    i2c_scl = (1 << scl);
+    i2c_address = address;
+    DDRB |= (i2c_sda | i2c_scl);              // outputs
+    PORTB |= (i2c_sda | i2c_scl);             // set all HIGH
+    wait_us(1);
+    i2c_start();
+    ret = i2c_write(i2c_address << 1);
     i2c_stop();
     return ret;
     }
 
-uint8_t i2c_init(uint8_t sda, uint8_t scl, uint8_t address){
-    i2c_sda = (1 << sda);
-    i2c_scl = (1 << scl);
-    DDRB |= (i2c_sda | i2c_scl);              // outputs
-    PORTB |= (i2c_sda | i2c_scl);             // set all HIGH
-    wait_us(5);
-    return i2c_write(address << 1);
+uint8_t i2c_write_reg(uint8_t reg, uint8_t value){
+	uint8_t ret;
+    i2c_start();
+    ret = i2c_write(i2c_address << 1);
+    if(ret==0) return 0;
+    ret = i2c_write(reg);
+    if(ret==0) return 0;
+    ret = i2c_write(value);
+    i2c_stop();    
+    if(ret==0) return 0;
+    return ret;
     }
+
+uint8_t i2c_read_reg(uint8_t reg){
+	uint8_t ret;
+    i2c_start();
+    ret = i2c_write(i2c_address << 1);
+    if(ret==0) return 0;
+    ret = i2c_write(reg);
+    if(ret==0) return 0;
+    
+    i2c_start();
+    ret = i2c_write((i2c_address << 1) | 1);
+    
+    ret = i2c_read();
+    i2c_stop();    
+    if(ret==0) return 0;
+    return ret;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
