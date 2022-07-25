30f98ea2b36956473a50ca9ce3e632cf2e97b101
Visualys <90142811+Visualys@users.noreply.github.com>
2022-07-25 13:37:53 +0200

    Update i2c_reader.c

--- 9.216 MHz/i2c_reader.c
+++ 9.216 MHz/i2c_reader.c
@@ -2,36 +2,18 @@
 #include <avr/eeprom.h>
 #include "lib_serial.c"
 #include "lib_wait.c"
-#include "lib_str.c"
 #include "lib_i2c.c"
 #include <stdio.h>
 
 char s[50];
-uint8_t v=0;
-uint32_t l=0;
+uint8_t v;
+uint16_t dig_t1;
+int16_t dig_t2, dig_t3;    // can be negative
+int32_t T[4], var1, var2;
 
-int16_t T[4];
-int32_t BME280t_fine;
-uint32_t adc;
-
-uint16_t t1;
-int16_t  t2, t3;
-uint8_t a, b, c;
-double d;
-
-uint32_t conv_unsigned16bit(uint8_t lsb, uint8_t msb){
-	uint32_t d = lsb | (msb << 8);
-	return d;
-	}
-
-double conv_signed16bit(uint8_t lsb, uint8_t msb){
-	double d = (int)(lsb | (msb << 8));
-	if(d>32767) d-=65536;
-	return d;
-	}
 
 void main(void) {
-    eeprom_update_byte((uint8_t*) 1, 140 );                          // Set the OSCCAL memory address (line to remove when set)
+    //eeprom_update_byte((uint8_t*) 1, 140 );                          // Set the OSCCAL memory address (line to remove when set)
     OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
     ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
     PRR |= ( 1 << PRADC );                                           // power off ADC
@@ -40,34 +22,28 @@ void main(void) {
     while(1){
 		v=i2c_init(PB0,PB1,0x76);
 		if(v){
-			serial_send(PA0, "BME connected.\n", 115200);
+            serial_send(PA0, "BME connected.\n", 115200);
+            
+			dig_t1 = i2c_read_reg(0x88) | (i2c_read_reg(0x89) << 8);
+			dig_t2 = i2c_read_reg(0x8A) | (i2c_read_reg(0x8B) << 8);
+			dig_t3 = i2c_read_reg(0x8C) | (i2c_read_reg(0x8D) << 8);
+            T[1]=dig_t1; T[2]=dig_t2; T[3]=dig_t3;
+            
+ 
 			i2c_write_reg(0xF2, 0b00000100);     // 8x sample
-			
-			
-			t1= i2c_read_reg(0x88) | (i2c_read_reg(0x89) << 8);
-			t2= i2c_read_reg(0x8A) | (i2c_read_reg(0x8B) << 8);
-			t3= i2c_read_reg(0x8C) | (i2c_read_reg(0x8D) << 8);
-			d = t3;
-			d = d + 0.25;
 			v = i2c_write_reg(0xF4, 0b10010001);                                   // start measurement                                     
             while(i2c_read_reg(0xF3) & 8);                                         // BME busy
  
-			
-			a=i2c_read_reg(0xFA);
-			b=i2c_read_reg(0xFB);
-			c=i2c_read_reg(0xFC);
-			         
-			         
-			sprintf(s, "%i %i %i %i\n", t1, t2, t3, d);               
+            T[0] = ((i2c_read_reg(0xFA)) << 12) | ((i2c_read_reg(0xFB)) << 4) | ((i2c_read_reg(0xFC)) >> 4);
+            var1 = ((T[0] >> 1) - (T[1] << 3)) * T[2];
+            var2 = (((T[0] >> 4) - T[1])*((T[0] >> 4) - T[1]) >> 13) * T[3] ;
+            T[0] = (var1+var2) / 419430;			
+            
+            
+			sprintf(s, "Temperature : %i\n");              
 			serial_send(PA0, s, 115200);
-			strset(s,"");
-			longtostr(d,s);
-			stradd(s,"\n");
-			serial_send(PA0, s, 115200);
-			sprintf(s, "-> %i %i %i \n", a, b, c);               
-			serial_send(PA0, s, 115200);
-			//serial_send(PA0, ".\n", 115200);
-			
+
+                                                                         
 			
 		}else{
 			serial_send(PA0, "BME not connected.\n", 115200);
