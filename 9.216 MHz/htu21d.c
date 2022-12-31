03f4382ca10abbb92874027cc222ec80dfe65559
Visualys <90142811+Visualys@users.noreply.github.com>
2022-12-31 11:53:53 +0100

    Add files via upload

---
+++ 9.216 MHz/htu21d.c
@@ -0,0 +1,37 @@
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include "lib_serial.c"
+#include "lib_wait.c"
+#include "lib_htu21d_PORTB.c"
+#include "lib_str.c"
+
+char s[32], st[6];
+uint16_t t=0;
+   
+void main(void) {
+    OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
+    ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
+    PRR |= ( 1 << PRADC );                                           // power off ADC
+    serial_send(PA5, "ATTiny84 started.\n", 115200);
+    while(1){
+		t=htu_read_temperature(PB0, PB1);
+		strset(s, "htu temperature : ");
+		longtostr(t,st);
+		stradd(s, st);
+		serial_send(PA5, s, 115200);
+		serial_send(PA5, "\n", 115200);	
+		
+		t=htu_read_humidity(PB0, PB1);				
+		strset(s, "htu humidity : ");
+		longtostr(t,st);
+		stradd(s, st);
+		serial_send(PA5, s, 115200);
+		serial_send(PA5, "\n", 115200);	
+        wait_ms(1000);	
+        }
+    }
+
+
+/*
+avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:htu21d.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
+*/
--- 9.216 MHz/template.c
+++ 9.216 MHz/template.c
@@ -2,21 +2,30 @@
 #include <avr/eeprom.h>
 #include "lib_serial.c"
 #include "lib_wait.c"
+#include "lib_str.c"
 
+// This Program is used to Synchronize CPU to 9.216 MHz
+
+char s[40];
+uint32_t v=0;
 
 void main(void) {
-    //eeprom_update_byte((uint8_t*) 1, 134 );                          // Set the OSCCAL memory address (line to remove when set)
     OSCCAL = eeprom_read_byte((uint8_t*)1) ;                         // read eeprom address 0x01
     ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
     PRR |= ( 1 << PRADC );                                           // power off ADC
     serial_send(PA0, "ATTiny84 started.\n", 115200);
     
     
-    PORTA |=0b00000110;
+    PORTA |=0b00000111;
     while(1){
+		if(!(PINA & 0b00000001)){eeprom_update_byte((uint8_t*) 1, OSCCAL);} // PA0 Save OSCCAL to EEPROM
 		if(!(PINA & 0b00000010)){OSCCAL++;}
 		if(!(PINA & 0b00000100)){OSCCAL--;}
-		serial_send(PA0, "...\n", 115200);
+		v=OSCCAL;
+		longtostr(v, s);
+		serial_send(PA5, "OSCCAL = ", 115200);
+		serial_send(PA5, s, 115200);
+		serial_send(PA5, "\n", 115200);
         wait_ms(1000);	
         }
     }
@@ -25,3 +34,7 @@ void main(void) {
 /*
 avrdude -p ATtiny84 -c stk500 -P /dev/ttyACM0 -U flash:w:template.hex:i -U lfuse:w:0xA2:m -U hfuse:w:0xD7:m
 */
+
+
+
+
