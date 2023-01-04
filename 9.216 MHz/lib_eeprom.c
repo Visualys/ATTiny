1458d5292abcacb48325570ee456c9356db5f9bb
Visualys <90142811+Visualys@users.noreply.github.com>
2023-01-04 15:28:59 +0100

    Create lib_eeprom.c

---
+++ 9.216 MHz/lib_eeprom.c
@@ -0,0 +1,24 @@
+#ifndef lib_eeprom
+#define lib_eeprom
+
+uint8_t eeprom_read(uint8_t addr){
+    while(EECR & (1<<EEPE));
+    EEARL = ucAddress;
+    EECR |= (1<<EERE);
+    return EEDR;
+    }
+
+void eeprom_write(uint8_t addr, uint8_t v){
+    while(EECR & (1<<EEPE));
+    EECR = (0<<EEPM1)|(0<<EEPM0)
+    EEARL = addr;
+    EEDR = v;
+    EECR |= (1<<EEMPE);
+    EECR |= (1<<EEPE);
+    }
+
+void eeprom_update(uint8_t addr, uint8_t v){
+    if(eeprom_read(addr) != v) eeprom_write(addr, v);
+    }
+
+#endif
