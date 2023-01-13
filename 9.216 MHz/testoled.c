e0057530b1ce53531765fc565e4ae00f962dd756
Visualys <90142811+Visualys@users.noreply.github.com>
2023-01-13 11:00:22 +0100

    Update testoled.c

--- 9.216 MHz/testoled.c
+++ 9.216 MHz/testoled.c
@@ -1,43 +1,55 @@
 #include <avr/io.h>
-#include "lib_eeprom.c"
-#include "lib_serial.c"
+//#include "lib_serial.c"
 #include "lib_wait.c"
 #include "lib_oled.c"
 #include "lib_str.c"
 #include "lib_htu21d_PORTB.c"
+#include "lib_rf24.c"
 
 char s[32], st[6];
 uint16_t t=0;
 uint8_t k=0;
+
    
 void main(void) {
-    OSCCAL = eeprom_read(1) ;                                        // read eeprom address 0x01
+    OSCCAL=139;
     ADCSRA &= ~( 1 << ADEN );                                        // set ADC off
     PRR |= ( 1 << PRADC );                                           // power off ADC
-    serial_send(PA5, "ATTiny84 started.\n", 115200);
-    wait_ms(150); // oled power up...
+    //serial_send(PA5, "ATTiny84 started.\n", 115200);
+    wait_ms(500); // oled power up...
 	t=oled_init(PB0, PB1);
-    while(1){
+	oled_clear(0);
+	oled_cmd(0xAF);               // display ONn
+	oled_cmd(0x81);	oled_cmd(32); // contrast
+	rf24_init(PA0,PA4,PA3,PA2,PA1);                                  // ce, cs, mosi, miso, clk
+    rf24_setconfig(90, 0, 3);
+    rf24_setaddress(0, 100,100,100,100,100);
+    rf24_setautoretransmit(15,15);
+    rf24_set_payload_length(32);
+
+	
+    while(1){	
+
+		k++;
+		if(k==9) k=0;
+		if(k==0) {
+			strset(s,"event,tbur=");
+			t=htu_read_temperature(PB0, PB1); longtostr(t,st); stradd(s, st);
+			stradd(s,",");
+			t=htu_read_humidity(PB0, PB1); longtostr(t,st); stradd(s, st);
+			stradd(s,"\n");
+			//serial_send(PA5, s, 115200);
+ 			oled_locate(0,6);
+ 			if(rf24_sendline(s)){
+				oled_print("send : OK. ",0);}
+				else{
+				oled_print("send : ERR!",0);
+				}
+			}
 
 		
-		//t=oled_command(); //display off
-		//t=oled_write(0xAE);
-		//oled_stop();
-	
-		if(k==0) oled_clear(0);
-		k++;
-
-
-		t=oled_command();
-		t=oled_write(0xB1); //page
-		oled_stop();
-		t=oled_command();
-		t=oled_write(0b00000010 );
-		oled_stop();		
-		t=oled_command();
-		t=oled_write(0b00010000);
-		oled_stop();		
-
+			
+		oled_gotoline(1);
 		t=htu_read_temperature(PB0, PB1);
 		strset(s, "  Temp : ");
 		longtostr(t,st);
@@ -46,31 +58,19 @@ void main(void) {
 		stradd(s,"°C ");
 		oled_print(s,0);
 
-		t=oled_command();
-		t=oled_write(0xB4); //page
-		oled_stop();
-		t=oled_command();
-		t=oled_write(0b00000010 );
-		oled_stop();		
-		t=oled_command();
-		t=oled_write(0b00010000);
-		oled_stop();		
-
+		oled_gotoline(3);	
 		t=htu_read_humidity(PB0, PB1);
 		strset(s, "   Hum : ");
 		longtostr(t,st);
 		st[5]=0;st[4]=st[3];st[3]=st[2];st[2]=46;
 		stradd(s, st);
-		stradd(s,"%");
+		stradd(s,"% ");
 		oled_print(s,0);
 
-
 		
-		t=oled_command(); //display on
-		t=oled_write(0xAF);
-		oled_stop();
+		
 
-
+			
         wait_ms(1000);	
         }
     }
