2ad74ab3913998f4d838ae5d07bc2de2c3ecfdcd
Visualys <90142811+Visualys@users.noreply.github.com>
2023-02-08 12:09:57 +0100

    Create CT_Measurer_2.c

---
+++ 9.216 MHz/CT_Measurer_2.c
@@ -0,0 +1,59 @@
+#include <avr/io.h>
+#include "lib_serial.c"
+#include "lib_wait.c"
+#include <stdio.h>
+
+
+// This Program is used to Measure SCT-013-030 on PA0.
+// Returning information (UART) on PA5.
+
+char s[50];
+uint16_t i, measures;
+double v;
+uint8_t n;
+int ent1, dec1, ent2, dec2;
+float w, conso, amp;
+
+uint16_t ADC_PA0_PA1vsVCC_read() {
+    uint16_t adc;
+    ADMUX = 0b10000000;                               // read PA1+/PA0- voltage vs VCC ref.
+    ADCSRB = 0;
+    ADCSRA = 0b10000000;                              // enable ADC
+    ADCSRA |= 0b01000000;                             // start ADC
+    while(ADCSRA & 0b01000000);                       // wait measure
+    adc = ADCL; adc |= ADCH << 8;
+    return adc;
+    }
+
+void main(void) {
+    OSCCAL = 134;
+    wait_ms(1000);
+    serial_send(PA5, "ATTiny84 started.\n", 115200);
+    conso=0;    
+    TCCR0A = 0b00000010;                              // CTC Mode
+    OCR0A = 180;                                      // 20ms
+    while(1){
+        for(n=0;n<50;n++){
+            v=0;
+            measures = 0;
+            TCNT0 = 0;
+            TCCR0B = 0b00000101;                      // start timer0 with 1/1024 speed		
+            while(!(TIFR0 & 0b00000010)){             // wait counter 0 overflow
+                i = ADC_PA0_PA1vsVCC_read();
+                v+=i;
+                measures++;
+                }
+            TIFR0 |= 0b00000111;                      // reset overflow flag
+            TCCR0B = 0;                               // stop timer0		
+            w = (float)(v / measures) * 19.0f;
+            conso += w / 180000.0f;
+            }
+        ent1 = w;
+        dec1 = 100.0f * (w-ent1);
+        ent2 = conso;
+        dec2 = 100.0f * (conso-ent2);
+        sprintf(s, "Puiss.: %i.%02d w, Conso.: %i.%02d Wh\r\n", ent1, dec1, ent2, dec2);
+        serial_send(PA5, s, 115200);
+        wait_ms(10);	
+        }
+    }
